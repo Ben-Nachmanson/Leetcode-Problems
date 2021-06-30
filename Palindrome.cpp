07afeb8e43adc06b0f84b2fad5953e59d4f5a204
Ben Nachmanson <bennachmanson@gmail.com>
2021-06-30 11:26:45 -0700

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Solution {
+public:
+    bool isPalindrome(int x) 
+    {
+       long n = x, num = x, digit, rev = 0;
+ 
+       if(num < 0)
+        {
+            return false;
+        }
+        
+       while(num!=0)
+       {
+           digit = num % 10;
+           rev = (rev*10) + digit;
+           num = num/10;
+       }
+        
+        return n == rev;
+    
+    }
+};
+
+int main()
+{
+    Solution x;
+    std::cout << x.isPalindrome(-121);
+
+}
