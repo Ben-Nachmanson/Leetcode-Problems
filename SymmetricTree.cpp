db0ad1be6e9c7d3cef0159e931d95e4f64fa7c0f
Ben-Nachmanson <bennachmanson@gmail.com>
2021-07-19 11:05:58 -0700

    adding problem

---
+++ SymmetricTree.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) 
+    {
+        if(root == NULL) return true;
+        return isSame(root->left, root->right);        
+    }
+    
+    bool isSame(TreeNode* left, TreeNode* right)
+    {
+        if(left == NULL)
+        {
+            return right == NULL ? true : false;
+        }
+        else
+        {
+            if( right == NULL) return false;
+            else
+            {
+                if(left->val == right-> val)return isSame(left->left,right->right)&&isSame(left->right,right->left);
+                else return false;
+                
+            }
+            
+        }
+    }
+};
